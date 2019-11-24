dde7bbe9bf512403124113483dde3097c987ca7b
vaibhav buccha <jainvaibhav415@gmail.com>
2019-11-24 22:12:39 +0530

    Create bool_datatypes.py

---
+++ bool_datatypes.py
@@ -0,0 +1,17 @@
+# :: boolean datatype ::
+
+# in python True and False are 2 boolean datatype
+
+a = True
+b = False
+
+print (type(a))
+print (type(b))
+
+# we can also perform arithmatic operations with boolean datatypes 
+#  True is concidered as 1 and false is concidered as 0
+
+print(a+b)
+print(a+a)
+print(a-b)
+print(a*b)
