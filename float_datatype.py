c77360e830e200e8f011c62e705da6c07500d2f4
vaibhav buccha <jainvaibhav415@gmail.com>
2019-11-24 21:50:38 +0530

    Create float_datatype.py

---
+++ float_datatype.py
@@ -0,0 +1,14 @@
+# :: float values ::
+
+# float values represnented in decimal only
+
+a  = 10.56
+print(type(a))
+
+a = 1.2e9
+print(type(a))
+print(a)
+
+# 1.2e9 = 1.2*10^3  (called scientific notation or exponential value)
+# it is used for represention of bigger values in smaller form.
+# and we can also store big values in small space .
